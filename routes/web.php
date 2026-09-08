1bf98d3844e6fea680f0214d8afedd39f66f22b9
daveenaalexandra <daveenaalexandrapentury@mail.ugm.ac.id>
2026-09-08 22:19:20 +0700

    revisi route

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CatatanEvolusiController;
+use Illuminate\Support\Facades\Route;
+
 
 Route::get('/', function () {
     return view('welcome');
